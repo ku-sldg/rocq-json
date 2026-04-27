e7e96850b3d28247a6c78a03f073b2c9bdf72a34
Will Thomas <30wthomas@gmail.com>
2026-04-27 12:30:55 -0500

    adding a little more test case output

--- theories/JSON_Derive.v
+++ theories/JSON_Derive.v
@@ -150,6 +150,13 @@ Example test_foo_roundtrip :
 Inductive tree := Leaf | Node (l : tree) (n : nat) (r : tree).
 Elpi derive.jsonifiable tree.
 Definition test_tree := Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf).
+Example test_tree_to_JSON :
+  to_JSON test_tree =
+    JSON_Object [("Node", JSON_Object [
+      ("l", to_JSON (Node Leaf 1 Leaf));
+      ("n", JSON_Nat 2);
+      ("r", to_JSON (Node Leaf 3 Leaf))])]
+  := eq_refl.
 Example test_tree_roundtrip :
   from_JSON (to_JSON test_tree) = res test_tree
   := canonical_jsonification test_tree.
@@ -162,6 +169,13 @@ Arguments JLeaf {A}.
 Arguments JNode {A} _ _ _.
 Elpi derive.jsonifiable jtree.
 Definition test_jtree := JNode 42 JLeaf JLeaf.
+Example test_jtree_to_JSON :
+  to_JSON test_jtree =
+    JSON_Object [("JNode", JSON_Object [
+      ("v", JSON_Nat 42);
+      ("j1", JSON_String "JLeaf");
+      ("j2", JSON_String "JLeaf")])]
+  := eq_refl.
 Example test_jtree_roundtrip :
   from_JSON (to_JSON test_jtree) = res test_jtree
   := canonical_jsonification test_jtree.
@@ -177,6 +191,13 @@ Arguments ABLeaf {A B}.
 Arguments ABNode {A B} _ _ _.
 Elpi derive.jsonifiable ab_tree.
 Definition test_ab_tree := ABNode 1 ABLeaf true.
+Example test_ab_tree_to_JSON :
+  to_JSON test_ab_tree =
+    JSON_Object [("ABNode", JSON_Object [
+      ("a", JSON_Nat 1);
+      ("tree'", JSON_String "ABLeaf");
+      ("b", JSON_Boolean true)])]
+  := eq_refl.
 Example test_ab_tree_roundtrip :
   from_JSON (to_JSON test_ab_tree) = res test_ab_tree
   := canonical_jsonification test_ab_tree.
@@ -189,6 +210,13 @@ Record test_rec := {
 }.
 Elpi derive.jsonifiable test_rec.
 Definition test_test_rec := {| a := 42; b := true; c := Node Leaf 1 Leaf |}.
+Example test_test_rec_to_JSON :
+  to_JSON test_test_rec =
+    JSON_Object [
+      ("a", JSON_Nat 42);
+      ("b", JSON_Boolean true);
+      ("c", to_JSON (Node Leaf 1 Leaf))]
+  := eq_refl.
 Example test_test_rec_roundtrip :
   from_JSON (to_JSON test_test_rec) = res test_test_rec
   := canonical_jsonification test_test_rec.
@@ -202,6 +230,13 @@ Record prec (A B : Type) := {
 Elpi derive.jsonifiable prec.
 Definition test_prec :=
   {| pa := 42; pb := true; pc := (ABNode 0 ABLeaf false) |}.
+Example test_prec_to_JSON :
+  to_JSON test_prec =
+    JSON_Object [
+      ("pa", JSON_Nat 42);
+      ("pb", JSON_Boolean true);
+      ("pc", to_JSON (ABNode 0 ABLeaf false))]
+  := eq_refl.
 Example test_prec_roundtrip :
   from_JSON (to_JSON test_prec) = res test_prec
   := canonical_jsonification test_prec.
@@ -219,6 +254,13 @@ Arguments NNode {A} _.
 Elpi derive.jsonifiable nested_tree.
 
 Definition test_nested_tree := NNode [(42, NLeaf); (7, NNode [(13, NLeaf)])].
+Example test_nested_tree_to_JSON :
+  to_JSON test_nested_tree =
+    JSON_Object [("NNode", JSON_Object [
+      ("l", JSON_Array [
+        JSON_Array [JSON_Nat 42; JSON_String "NLeaf"];
+        JSON_Array [JSON_Nat 7; to_JSON (NNode [(13, NLeaf)] : nested_tree nat)]])])]
+  := eq_refl.
 Example test_nested_tree_roundtrip :
   from_JSON (to_JSON test_nested_tree) = res test_nested_tree
   := canonical_jsonification test_nested_tree.
@@ -232,6 +274,16 @@ Arguments NNode2 {A B} _ _.
 Elpi derive.jsonifiable nested_tree_2.
 
 Definition test_nested_tree_2 := NNode2 [(42, NLeaf2); (7, NNode2 [(13, NLeaf2)] [])] [(true, NLeaf2); (false, NNode2 [] [(true, NLeaf2)])].
+Example test_nested_tree_2_to_JSON :
+  to_JSON test_nested_tree_2 =
+    JSON_Object [("NNode2", JSON_Object [
+      ("l1", JSON_Array [
+        JSON_Array [JSON_Nat 42; JSON_String "NLeaf2"];
+        JSON_Array [JSON_Nat 7; to_JSON (NNode2 [(13, NLeaf2)] [] : nested_tree_2 nat bool)]]);
+      ("l2", JSON_Array [
+        JSON_Array [JSON_Boolean true; JSON_String "NLeaf2"];
+        JSON_Array [JSON_Boolean false; to_JSON (NNode2 [] [(true, NLeaf2)] : nested_tree_2 nat bool)]])])]
+  := eq_refl.
 Example test_nested_tree_2_roundtrip :
   from_JSON (to_JSON test_nested_tree_2) = res test_nested_tree_2
   := canonical_jsonification test_nested_tree_2.
@@ -250,9 +302,18 @@ Inductive many_constructors :=
 Elpi derive.jsonifiable many_constructors.
 Definition test_many_constructors := MC8 (NNode2 [(42, NLeaf2); (7, NNode2 [(13, NLeaf2)] [])] [(true, NLeaf2); (false, NNode2 [] [(true, NLeaf2)])]).
 Definition test_many_constructors2 := MC4 [1; 2; 3; 4].
+Example test_many_constructors_to_JSON :
+  to_JSON test_many_constructors =
+    JSON_Object [("MC8", JSON_Object [("nt2", to_JSON test_nested_tree_2)])]
+  := eq_refl.
 Example test_many_constructors_roundtrip :
   from_JSON (to_JSON test_many_constructors) = res test_many_constructors
   := canonical_jsonification test_many_constructors.
+Example test_many_constructors2_to_JSON :
+  to_JSON test_many_constructors2 =
+    JSON_Object [("MC4", JSON_Object [
+      ("l", JSON_Array [JSON_Nat 1; JSON_Nat 2; JSON_Nat 3; JSON_Nat 4])])]
+  := eq_refl.
 Example test_many_constructors2_roundtrip :
   from_JSON (to_JSON test_many_constructors2) = res test_many_constructors2
   := canonical_jsonification test_many_constructors2.
@@ -264,6 +325,12 @@ Inductive option_payload :=
 Elpi derive.jsonifiable option_payload.
 
 Definition test_option_payload := OPMix (Some 5) None.
+Example test_option_payload_to_JSON :
+  to_JSON test_option_payload =
+    JSON_Object [("OPMix", JSON_Object [
+      ("n", to_JSON (Some 5));
+      ("b", to_JSON (None : option bool))])]
+  := eq_refl.
 Example test_option_payload_roundtrip :
   from_JSON (to_JSON test_option_payload) = res test_option_payload.
 Proof. exact (canonical_jsonification test_option_payload). Qed.
@@ -277,6 +344,12 @@ Elpi derive.jsonifiable option_rec.
 
 Definition test_option_rec :=
   {| or_n := Some 42; or_t := Some (Node Leaf 1 Leaf) |}.
+Example test_option_rec_to_JSON :
+  to_JSON test_option_rec =
+    JSON_Object [
+      ("or_n", to_JSON (Some 42));
+      ("or_t", to_JSON (Some (Node Leaf 1 Leaf)))]
+  := eq_refl.
 Example test_option_rec_roundtrip :
   from_JSON (to_JSON test_option_rec) = res test_option_rec.
 Proof. exact (canonical_jsonification test_option_rec). Qed.
@@ -291,6 +364,14 @@ Elpi derive.jsonifiable nested_tree_3.
 
 Definition test_nested_tree_3 : nested_tree_3 nat bool :=
   NNode3 43 true (Some (NNode3 7 false None)).
+Example test_nested_tree_3_to_JSON :
+  to_JSON test_nested_tree_3 =
+    JSON_Object [("NNode3", JSON_Object [
+      ("v1", JSON_Nat 43);
+      ("v2", JSON_Boolean true);
+      ("o", JSON_Object [("Some", JSON_Object [
+        ("v", to_JSON (NNode3 7 false None : nested_tree_3 nat bool))])])])]
+  := eq_refl.
 Example test_nested_tree_3_roundtrip :
   from_JSON (to_JSON test_nested_tree_3) = res test_nested_tree_3.
 Proof. exact (canonical_jsonification test_nested_tree_3). Qed.
@@ -305,6 +386,18 @@ Elpi derive.jsonifiable nested_tree_4.
 
 Definition test_nested_tree_4 : nested_tree_4 nat bool string :=
   NNode4 [(42, Some NLeaf4); (7, Some (NNode4 [(13, None)]))].
+Example test_nested_tree_4_to_JSON :
+  to_JSON test_nested_tree_4 =
+    JSON_Object [("NNode4", JSON_Object [
+      ("l", JSON_Array [
+        JSON_Array [
+          JSON_Nat 42;
+          JSON_Object [("Some", JSON_Object [("v", JSON_String "NLeaf4")])]];
+        JSON_Array [
+          JSON_Nat 7;
+          JSON_Object [("Some", JSON_Object [
+            ("v", to_JSON (NNode4 [(13, None)] : nested_tree_4 nat bool string))])]]])])]
+  := eq_refl.
 Example test_nested_tree_4_roundtrip :
   from_JSON (to_JSON test_nested_tree_4) = res test_nested_tree_4.
 Proof. exact (canonical_jsonification test_nested_tree_4). Qed.
